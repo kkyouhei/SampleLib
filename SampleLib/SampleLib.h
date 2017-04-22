3f2059866ede66c93adad70748e6f6304cc4fea4
kkyouhei <hope637@gmail.com>
2017-04-22 23:41:16 +0900

    first commit

---
+++ SampleLib/SampleLib.h
@@ -0,0 +1,19 @@
+//
+//  SampleLib.h
+//  SampleLib
+//
+//  Created by 狩野 恭平 on 2017/04/22.
+//  Copyright © 2017年 狩野 恭平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleLib.
+FOUNDATION_EXPORT double SampleLibVersionNumber;
+
+//! Project version string for SampleLib.
+FOUNDATION_EXPORT const unsigned char SampleLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleLib/PublicHeader.h>
+
+
