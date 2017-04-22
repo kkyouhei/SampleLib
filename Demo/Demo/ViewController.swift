0793090520677181b7d469bcb5539148c351b827
kkyouhei <hope637@gmail.com>
2017-04-23 00:27:45 +0900

    add use library code

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let hoge = SampleLib()
+        hoge.hello()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +21,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
---
+++ SampleLib/SampleLib.swift
@@ -0,0 +1,15 @@
+//
+//  SampleLib.swift
+//  Demo
+//
+//  Created by 狩野 恭平 on 2017/04/23.
+//  Copyright © 2017年 狩野 恭平. All rights reserved.
+//
+
+import Foundation
+
+open class SampleLib{
+    open func hello(){
+        print("hello world")
+    }
+}
